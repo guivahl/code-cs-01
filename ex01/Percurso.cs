f02fa08b96186c3c13baf1dc9ec6edea33876427
Guilherme dos Santos Vahl <gdsvahl@inf.ufpel.edu.br>
2022-12-20 16:44:14 -0300

    feat: metodo Preencher na classe Percurso

--- ex01/Percurso.cs
+++ ex01/Percurso.cs
@@ -5,10 +5,32 @@ namespace ex01
         private List<int> trajeto = new List<int>();
         public int Distancia { get; private set; }
         public string Trajeto { get { return String.Join(",", this.trajeto); } }
-        public void AdicionaCidade(int cidade) => trajeto.Add(cidade); 
-        public void AdicionaDistancia(int distancia) => this.Distancia += distancia;
-        public int Tamanho () => this.trajeto.Count;
+        private void AdicionaCidade(int cidade) => trajeto.Add(cidade); 
+        private void AdicionaDistancia(int distancia) => this.Distancia += distancia;
+        private int Tamanho () => this.trajeto.Count;
         public int UltimaCidade () => this.trajeto.Last();
+
+        public void Preencher(Mapa mapa) {
+            System.Console.Write("Qual cidade você está? ");
+            
+            int proximaCidade = System.Console.ReadLine().ToInt();
+
+            while (mapa.EhCidadeValida(proximaCidade))  {
+                if (this.Tamanho() > 0) {
+                    int cidadeOrigem = this.UltimaCidade();
+                    int distancia = mapa.ConsultaDistancia(cidadeOrigem, proximaCidade);
+                    
+                    this.AdicionaDistancia(distancia);
+                }
+
+                this.AdicionaCidade(proximaCidade);
+
+                System.Console.WriteLine($"Percurso atualizado: {this.Trajeto}");            
+                System.Console.Write("Qual próxima cidade? (para Parar, digite um valor inválido.) ");
+            
+                proximaCidade = System.Console.ReadLine().ToInt();
+            }
+        }
 
         public override string ToString() => $"Trajeto {this.Trajeto} = {this.Distancia} km";
     }
--- ex01/Program.cs
+++ ex01/Program.cs
@@ -16,25 +16,7 @@ class Program
 
         Percurso percurso = new Percurso();
 
-        System.Console.Write("Qual cidade você está? ");
-        
-        int proximaCidade = System.Console.ReadLine().ToInt();
-
-        while (mapa.EhCidadeValida(proximaCidade))  {
-            if (percurso.Tamanho() > 0) {
-                int cidadeOrigem = percurso.UltimaCidade();
-                int distancia = mapa.ConsultaDistancia(cidadeOrigem, proximaCidade);
-                
-                percurso.AdicionaDistancia(distancia);
-            }
-
-            percurso.AdicionaCidade(proximaCidade);
-
-            System.Console.WriteLine($"Percurso atualizado: {percurso.Trajeto}");            
-            System.Console.Write("Qual próxima cidade? (para Parar, digite um valor inválido.) ");
-           
-            proximaCidade = System.Console.ReadLine().ToInt();
-        }
+        percurso.Preencher(mapa);
 
         System.Console.Write(mapa.ToString());
         System.Console.WriteLine($"Trajeto (cidades): {percurso.Trajeto}");
