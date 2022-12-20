128f6bb84cc72661fc559f75af2279c4edf107e1
Guilherme dos Santos Vahl <gdsvahl@inf.ufpel.edu.br>
2022-12-20 19:59:57 -0300

    feat: metodo na classe util para transformar array de strings em array de inteiros

--- ex01/Util.cs
+++ ex01/Util.cs
@@ -4,5 +4,8 @@ namespace ex01
     {
         public static int ToInt(this string? str) =>
             int.TryParse(str, out int number) ? number : default;
+
+        public static int[] ToInt(this string[] arrStr) =>
+            arrStr.Select(int.Parse).ToArray();
     }
 }
