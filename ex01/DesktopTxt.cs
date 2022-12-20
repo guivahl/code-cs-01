7ca237581785e4344490c0e901456dd9676ec6b1
Guilherme dos Santos Vahl <gdsvahl@inf.ufpel.edu.br>
2022-12-20 19:59:35 -0300

    feat: classe para manipular arquivos txt no desktop

---
+++ ex01/DesktopTxt.cs
@@ -0,0 +1,28 @@
+namespace ex01
+{
+    public class DesktopTxt
+    {
+        private string caminhoDesktop = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+        private string caminhoArquivo;
+
+        public DesktopTxt(string nomeArquivo)
+        {
+            string caminhoArquivo = Path.Join(caminhoDesktop, nomeArquivo);
+
+            this.caminhoArquivo = caminhoArquivo;
+        }
+
+        public bool ExisteArquivo() => new FileInfo(caminhoArquivo).Exists;
+
+        public string LerTexto() => File.ReadAllText(this.caminhoArquivo);
+
+        public int QuantidadeLinhas() => File.ReadLines(this.caminhoArquivo).Count();
+
+        public string[] ObterValores() =>
+            this.LerTexto()
+                    .Replace(Environment.NewLine, ",")
+                    .Split(",")
+                    .Where(valor => Int32.TryParse(valor, out int x))
+                    .ToArray();
+    }
+}
